e23dc4fba692587838e274c482a865e271a7bfe7
Sami IMEGHRI <IMEGHRISAMI@gmail.com>
2022-05-22 05:45:31 +0100

    create docker terminal UI and link it with the backend service

--- client/Router.js
+++ client/Router.js
@@ -1,22 +1,22 @@
-import { Provider } from 'mobx-react'
-import React from 'react'
+import { Provider } from "mobx-react";
+import React from "react";
 import {
   Router as ReactRouter,
   Route,
   browserHistory,
   IndexRedirect,
-} from 'react-router'
-import AppStore from 'stores/AppStore'
-import Login from './components/Login/Login'
-import App from './components/App'
-import Images from './components/Images/Images'
-import Containers from './components/Containers/Containers'
-import Volumes from './components/Volumes/Volumes'
-import Networks from './components/Networks/Networks'
-import Logs from './components/Logs/Logs'
-import ContainerStat from './components/ContainerStats/ContainerStats'
-import ContainerRLTStats from './components/ContainerStats/ContainerRLTStats'
-import DockerFileGen from './components/dockerfileGenerator/DockerFileGen'
+} from "react-router";
+import AppStore from "stores/AppStore";
+import Login from "./components/Login/Login";
+import App from "./components/App";
+import Images from "./components/Images/Images";
+import Containers from "./components/Containers/Containers";
+import Volumes from "./components/Volumes/Volumes";
+import Networks from "./components/Networks/Networks";
+import Logs from "./components/Logs/Logs";
+import ContainerStat from "./components/ContainerStats/ContainerStats";
+import DockerFileGen from "./components/dockerfileGenerator/DockerFileGen";
+import DockerTerminal from "./components/DockerTerminal/DockerTerminal";
 
 export default class Router extends React.Component {
   props: {
@@ -28,19 +28,19 @@ export default class Router extends React.Component {
       <Provider store={this.props.store}>
         <ReactRouter history={browserHistory}>
           <Route path="/" component={App}>
-            <IndexRedirect to="images" />
+            <IndexRedirect to="container-stats" />
             <Route path="login" component={Login} />
             <Route path="images" component={Images} />
             <Route path="containers" component={Containers} />
             <Route path="container-stats" component={ContainerStat} />
             <Route path="dockerfile-generator" component={DockerFileGen} />
-            <Route path="container-stats/:id" component={ContainerRLTStats} />
+            <Route path="docker-terminal/:id" component={DockerTerminal} />
             <Route path="volumes" component={Volumes} />
             <Route path="networks" component={Networks} />
             <Route path="Logs" component={Logs} />
           </Route>
         </ReactRouter>
       </Provider>
-    )
+    );
   }
 }
